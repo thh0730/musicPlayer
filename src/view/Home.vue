eda98b881d316de31588904de11e9a4cf9545836
Shutting <309063773@qq.com>
2020-04-14 20:46:49 +0800

    Initial commit

---
+++ src/view/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <player></player>
+  </div>
+</template>
+
+<script>
+import Player from "../components/Player";
+export default {
+  name: "Home",
+  components: {
+    Player
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.home {
+  position: relative;
+  z-index: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  background-color: #ffedff;
+}
+</style>
